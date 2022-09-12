ed6b73075d31174a377ee06ec76420aa9c6ac78f
Shubh Bapna <38372682+shubhbapna@users.noreply.github.com>
2022-09-12 14:56:47 -0400

    Jest coverage (#5)
    * reconfigured jest and removed unnecessary dependencies being used to produce coverage reports
    
    * removed sonar for now

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,16 @@ const jestConfig: Config.InitialOptions = {
     "^.+\\.tsx?$": "ts-jest",
   },
   clearMocks: true,
-  resetMocks: true
+  resetMocks: true,
+  collectCoverageFrom: [
+    "src/**",
+    "!**/*.types.ts",
+    "!**/*.constants.ts",
+    "!src/action-compiler/mocker.ts",
+    "!src/index.ts"
+  ],
+  testLocationInResults: true,
+  testResultsProcessor: "jest-sonar-reporter",
+  testTimeout: 10000
 };
 export default jestConfig;
