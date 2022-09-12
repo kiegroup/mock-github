8ffad8c72d5b8d2240e4b6f107ebfa20402b6eda
Github <sbapna@redhat.com>
2022-09-12 13:20:31 -0400

    reconfigured jest and removed unnecessary dependencies being used to produce coverage reports

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,15 @@ const jestConfig: Config.InitialOptions = {
     "^.+\\.tsx?$": "ts-jest",
   },
   clearMocks: true,
-  resetMocks: true
+  resetMocks: true,
+  collectCoverageFrom: [
+    "src/**",
+    "!**/*.types.ts",
+    "!**/*.constants.ts",
+    "!src/action-compiler/mocker.ts"
+  ],
+  coverageReporters: ["lcov", "json", "text"],
+  testLocationInResults: true,
+  testTimeout: 10000
 };
 export default jestConfig;
--- test/action-compiler/action-compiler.test.ts
+++ test/action-compiler/action-compiler.test.ts
@@ -1,8 +1,21 @@
-import { spawnSync } from "child_process";
+import { execSync, spawnSync } from "child_process";
 import { rmSync } from "fs";
 import path from "path";
+import { promisify } from "util";
 import { ActionCompiler } from "../../src/action-compiler/action-compile";
 import { CompileMockAPIs } from "../../src/action-compiler/action-compile.types";
+
+beforeAll(async () => {
+  const exec = promisify(require("node:child_process").exec);
+  await Promise.all([
+    exec("npm run pre-compiler-test:minify"),
+    exec("npm run pre-compiler-test:no-minify"),
+  ]);
+});
+
+afterAll(async () => {
+  execSync("npm run post-compiler-test")
+})
 
 test.each([
   ["minified", true, "dist-minify/index.js"],
