6853694a39329d4a15ae9f04528f60828bed735c
Github <sbapna@redhat.com>
2022-09-12 11:56:53 -0400

    cleaned up repository mocker

--- src/mock/repository/repository-mocker.types.ts
+++ src/mock/repository/repository-mocker.types.ts
@@ -2,15 +2,16 @@ import { CreateRepositoryFile } from "./files/repository-file-system.types";
 import { GitAction } from "./history/repository-history.types";
 import { PullRequests } from "./state/repository-state.types";
 
+export type Repository = {
+  pushedBranches?: string[];
+  localBranches?: string[];
+  currentBranch?: string;
+  history?: GitAction[];
+  owner?: string;
+  files?: CreateRepositoryFile[];
+  forkedFrom?: string;
+  pullRequests?: PullRequests;
+};
 export type Repositories = {
-  [key: string]: {
-    pushedBranches?: string[];
-    localBranches?: string[];
-    currentBranch?: string;
-    history?: GitAction[];
-    owner?: string;
-    files?: CreateRepositoryFile[];
-    forkedFrom?: string;
-    pullRequests?: PullRequests;
-  };
+  [key: string]: Repository
 };
--- src/mock/repository/repository.constants.ts
+++ src/mock/repository/repository.constants.ts
@@ -6,3 +6,4 @@ export const DUMMY_FILE_NAME = "dummy-file-";
 export const DUMMY_FILE_DATA = "dummy data"
 export const DEFAULT_COMMIT_MSG = (num: number) => `adding files to mimic history at index ${num}`;
 export const DEFAULT_MERGE_COMMIT_MSG = (base: string, head: string) => `Merging ${head} to ${base}`;
+export const DEFAULT_INIT_COMMIT_MSG = "initialization";
