4c067ba44ade0ddfe58de380aac6e30487260ab7
Github <sbapna@redhat.com>
2022-09-26 10:34:12 -0400

    added scripts to generate endpoints from open api and map them to request mocker objects

---
+++ scripts/api-spec.js
@@ -0,0 +1,73 @@
+const { writeFileSync } = require("fs");
+const path = require("path");
+
+const graphql = require("github-openapi-graphql-query");
+const prettier = require("prettier");
+
+const version = process.env.VERSION?.replace(/^v/, "") ?? "7.14.0";
+
+const QUERY = `
+  query ($version: String!, $ignoreChangesBefore: String!) {
+    endpoints(version: $version, ignoreChangesBefore: $ignoreChangesBefore, filter: { isGithubCloudOnly: false }) {
+      name
+      scope(format: CAMELCASE)
+      id(format: CAMELCASE)
+      method
+      url
+      isDeprecated
+      deprecationDate
+      removalDate
+      description
+      documentationUrl
+      parameters {
+        name
+        description
+        in
+        type
+        required
+        enum
+        allowNull
+        mapToData
+        validation
+        alias
+        deprecated
+      }
+      responses {
+        code
+        description
+        examples {
+          data
+        }
+      }
+      renamed {
+        before {
+          scope(format: CAMELCASE)
+          id(format: CAMELCASE)
+        }
+        after {
+          scope(format: CAMELCASE)
+          id(format: CAMELCASE)
+        }
+        date
+        note
+      }
+    }
+  }`;
+
+main();
+
+async function main() {
+  const {
+    data: { endpoints },
+  } = await graphql(QUERY, {
+    version,
+    ignoreChangesBefore: "2020-06-10",
+  });
+
+  writeFileSync(
+    path.resolve(__dirname, "generated", "endpoints.json"),
+    prettier.format(JSON.stringify(endpoints), {
+      parser: "json",
+    })
+  );
+}
---
+++ scripts/util.js
@@ -0,0 +1,36 @@
+module.exports = {
+  isDeprecated,
+};
+
+/**
+ * we ignore all legacy endpoints except the ones that were recently added
+ * @param { Endpoint } endpoint
+ */
+function isDeprecated(endpoint) {
+  if (
+    endpoint.removalDate &&
+    endpoint.removalDate < new Date().toISOString().substr(0, 10)
+  ) {
+    return true;
+  }
+
+  const deprecated =
+    !endpoint.removalDate && (endpoint.isLegacy || endpoint.isDeprecated);
+
+  if (!deprecated) {
+    return false;
+  }
+
+  if (endpoint.isLegacy && /^\/teams\/\{team_id\}/.test(endpoint.url)) {
+    return false;
+  }
+
+  if (
+    endpoint.method === "DELETE" &&
+    endpoint.url === "/reactions/{reaction_id}"
+  ) {
+    return false;
+  }
+
+  return true;
+}
