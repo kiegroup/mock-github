59f5eed238d223a4593ebedd28277677d47bcccf
Github <sbapna@redhat.com>
2022-09-30 10:57:10 -0400

    bug fixes in archive server

--- src/github/action/archive/archive-mocker.ts
+++ src/github/action/archive/archive-mocker.ts
@@ -1,3 +1,5 @@
+import { existsSync, rm } from "fs-extra";
+import path from "path";
 import { Mocker } from "../../mocker";
 import { ArchiveArtifactsInterface } from "./archive-mocker.types";
 import { ArchiveServer } from "./archive-server";
@@ -9,24 +11,30 @@ export class ArchiveArtifactsMocker
   private store: string;
   private runId: string;
   private port: string;
+  private isStoreCreated: boolean;
 
-  constructor(store: string = __dirname, port: string = "8000") {
+  constructor(store: string = path.join(__dirname, "store"), port: string = "8000") {
     this.archiveServer = new ArchiveServer(store, port);
     this.store = store;
     this.runId = Math.floor(Math.random() * 100).toString();
     this.port = port;
+    this.isStoreCreated = !existsSync(this.store);
   }
 
   async setup(): Promise<void> {
     await this.archiveServer.start();
-    process.env["ACTIONS_RUNTIME_URL"] = `http://localhost:${this.port}`;
+
+    // need the trailing slash cause @actions/artifact does not add one
+    process.env["ACTIONS_RUNTIME_URL"] = `http://localhost:${this.port}/`;
     process.env["GITHUB_RUN_ID"] = this.runId;
+    process.env["ACTIONS_RUNTIME_TOKEN"] = "token"
   }
 
   async teardown(): Promise<void> {
-    await this.archiveServer.stop();
+    await Promise.all([this.archiveServer.stop(), ...(this.isStoreCreated ? [rm(this.store)] : [])])
     delete process.env["ACTIONS_RUNTIME_URL"];
     delete process.env["GITHUB_RUN_ID"];
+    delete process.env["ACTIONS_RUNTIME_TOKEN"]
   }
 
   getArtifactStore(): string {
