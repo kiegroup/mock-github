dcb6712153f2f0284d857b9429bc05aa3ee76b55
Github <sbapna@redhat.com>
2022-10-03 18:40:01 -0400

    added test cases for act

--- src/act/act.ts
+++ src/act/act.ts
@@ -7,7 +7,7 @@ export class Act {
   constructor(cwd?: string) {
     // check if act exists
     try {
-      this.act(__dirname, "--verison");
+      this.act(__dirname, "--version");
       this.storedSecrets = {};
       this.cwd = cwd ?? process.cwd();
     } catch (err) {
@@ -17,44 +17,13 @@ export class Act {
     }
   }
 
-  // wrapper around the act cli command
-  private act(cwd: string, ...args: string[]) {
-    const response = spawnSync("act", args, { cwd });
-    if (response.status === null) {
-      throw new Error(response.error?.message);
-    }
-    return response;
-  }
-
   setCwd(cwd: string) {
     this.cwd = cwd;
+    return this;
   }
 
-  /**
-   * List available workflows.
-   * If working directory is not specified then node's current working directory is used
-   * @param cwd
-   */
-  async list(cwd: string = this.cwd): Promise<Workflow[]> {
-    const response = this.act(cwd, "-l");
-    if (response.status !== 0) {
-      throw new Error(response.stderr.toString());
-    }
-    const output = response.stdout
-      .toString()
-      .split("\n")
-      .slice(1, -1) // remove first (title columns) and last column (empty strings)
-      .map((element) => {
-        const splitElement = element.split("  ").filter((val) => val !== ""); // remove empty strings
-        return {
-          jobId: splitElement[1].trim(),
-          jobName: splitElement[2].trim(),
-          workflowName: splitElement[3].trim(),
-          workflowFile: splitElement[4].trim(),
-          events: splitElement[5].trim(),
-        };
-      });
-    return output;
+  getCwd() {
+    return this.cwd;
   }
 
   setSecret(key: string, val: string): Act {
@@ -70,6 +39,56 @@ export class Act {
   clearSecrets(): Act {
     this.storedSecrets = {};
     return this;
+  }
+
+  /**
+   * List available workflows.
+   * If working directory is not specified then node's current working directory is used
+   * You can also list workflows specific to an event by passing the event name
+   * @param cwd
+   * @param event
+   */
+  async list(event?: string, cwd: string = this.cwd): Promise<Workflow[]> {
+    const response = this.act(cwd, ...(event ? [event, "-l"] : ["-l"]));
+
+    const output = response
+      .split("\n")
+      .slice(1, -1) // remove first (title columns) and last column
+      .filter((element) => element !== "" && element.split("  ").length > 1) // remove empty strings and warnings
+      .map((element) => {
+        const splitElement = element.split("  ").filter((val) => val !== ""); // remove emoty strings
+        return {
+          jobId: splitElement[1].trim(),
+          jobName: splitElement[2].trim(),
+          workflowName: splitElement[3].trim(),
+          workflowFile: splitElement[4].trim(),
+          events: splitElement[5].trim(),
+        };
+      });
+    return output;
+  }
+
+  async runJob(jobId: string, cwd: string = this.cwd): Promise<Job[]> {
+    return this.run(["-j", jobId], cwd);
+  }
+
+  async runEvent(event: string, cwd: string = this.cwd): Promise<Job[]> {
+    return this.run([event], cwd);
+  }
+
+  // wrapper around the act cli command
+  private act(cwd: string, ...args: string[]) {
+    const response = spawnSync("act", ["-W", cwd, ...args]);
+    if (response.status === null) {
+      throw new Error(response.error?.message);
+    }
+    return response.stdout.toString();
+  }
+
+  private async run(cmd: string[], cwd: string): Promise<Job[]> {
+    const secrets = this.generateSecrets();
+    const response = this.act(cwd, ...cmd, ...secrets);
+    return this.extractRunOutput(response);
   }
 
   /**
@@ -105,11 +124,19 @@ export class Act {
          * even completely resassigning job variable and then updating
          * still affects the previous pushes
          */
-        result.push({ ...job[successMatcherResult[1]] });
+        result.push({
+          ...job[successMatcherResult[1]],
+          name: job[successMatcherResult[1]].name.trim(),
+          output: job[successMatcherResult[1]].output.trim(),
+        });
         job[successMatcherResult[1]] = { ...DEFAULT_JOB };
       } else if (failureMatcherResult !== null) {
         job[failureMatcherResult[1]].status = 1;
-        result.push({ ...job[failureMatcherResult[1]] });
+        result.push({
+          ...job[failureMatcherResult[1]],
+          name: job[failureMatcherResult[1]].name.trim(),
+          output: job[failureMatcherResult[1]].output.trim(),
+        });
         job[failureMatcherResult[1]] = { ...DEFAULT_JOB };
       } else if (runOutputMatcherResult !== null) {
         job[runOutputMatcherResult[1]].output +=
@@ -128,21 +155,5 @@ export class Act {
       arr.push("-s", `${key}=${this.storedSecrets[key]}`);
       return arr;
     }, []);
-  }
-
-  private async run(cmd: string[], cwd: string): Promise<Job[]> {
-    const secrets = this.generateSecrets();
-    const response = this.act(cwd, ...cmd, ...secrets);
-    return this.extractRunOutput(response.stdout.toString());
-  }
-
-  async runJob(jobId: string, cwd: string = this.cwd, dryRun: boolean = false): Promise<Job[]> {
-    const cmd = dryRun ? ["-n", "-j", jobId] : ["-j", jobId];
-    return this.run(cmd, cwd);
-  }
-
-  async runEvent(event: string, cwd: string = this.cwd, dryRun: boolean = false): Promise<Job[]> {
-    const cmd = dryRun ? ["-n", event] : [event];
-    return this.run(cmd, cwd);
   }
 }
