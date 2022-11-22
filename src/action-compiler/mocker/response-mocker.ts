7d8db3723dfb19d5def3d1ae28eb7a1a60365be4
Shubh Bapna <sbapna@redhat.com>
2022-11-22 18:11:36 -0500

    removed action-compiler

--- src/action-compiler/mocker/response-mocker.ts
+++
@@ -1,4 +0,0 @@
-import { ResponseMocker } from "../../endpoint-mocker/response/abstract-response-mocker";
-import nock from "nock/types";
-
-export class CompilerResponseMocker extends ResponseMocker<nock.Body, number> {}
