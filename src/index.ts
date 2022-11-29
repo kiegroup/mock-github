c3a3ccbd531874b2b0babdee143673097e980038
Shubh Bapna <sbapna@redhat.com>
2022-11-29 16:21:17 -0500

    expose types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
 import { MockGithub } from "@mg/github/github-mocker";
 import { Moctokit } from "@mg/moctokit/moctokit";
 import { Mockapi } from "@mg/mockapi/mockapi";
+import {
+  FileState,
+  State,
+  BranchState,
+} from "@mg/github/repository/state/repository-state.types";
+import {
+  Push,
+  Merge,
+  GitActionTypes,
+} from "@mg/github/repository/history/repository-history.types";
+import { CreateRepositoryFile } from "@mg/github/repository/files/repository-file-system.types";
+import { Env } from "@mg/github/env/env-mocker.types";
+import { Input } from "@mg/github/action/input/input-mocker.types";
+import { Response } from "@mg/endpoint-mocker/response/abstract-response-mocker.types";
 
-export { MockGithub, Mockapi, Moctokit };
+export {
+  MockGithub,
+  Mockapi,
+  Moctokit,
+  FileState,
+  State,
+  BranchState,
+  Push,
+  Merge,
+  GitActionTypes,
+  CreateRepositoryFile,
+  Env,
+  Input,
+  Response,
+};
