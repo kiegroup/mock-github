942b3c65ce335c4d76945937d87c936797be7ded
Shubh Bapna <sbapna@redhat.com>
2023-01-17 16:55:33 -0500

    removed mockapi

--- src/mockapi/response/response-mocker.ts
+++
@@ -1,4 +0,0 @@
-import { ResponseMocker } from "@mg/endpoint-mocker/response/abstract-response-mocker";
-import nock from "nock/types";
-
-export class MockapiResponseMocker extends ResponseMocker<nock.Body, number> {}
